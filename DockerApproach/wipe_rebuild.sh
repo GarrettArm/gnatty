176fe88d64f85e52034d6c9332a9e9695c1d6f6e
GarrettArm <garrett.w.armstrong@gmail.com>
2019-03-31 18:19:28 -0500

    better names

---
+++ DockerApproach/wipe_rebuild.sh
@@ -0,0 +1,10 @@
+docker-compose down
+docker ps
+docker volume prune -f
+docker network prune -f
+docker system prune  -f
+docker volume ls
+docker network ls
+docker ps -a
+docker-compose build --no-cache
+docker-compose up
