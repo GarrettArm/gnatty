887591ffab325bfc0f290193b226afc15f85719b
GarrettArm <garrett.armstrong@gmail.com>
2019-04-01 15:05:34 -0500

    circleCI approach to building box

--- DockerApproach/wipe_rebuild.sh
+++ DockerApproach/wipe_rebuild.sh
@@ -1,4 +1,8 @@
 docker-compose down
+echo "removing DrupalCode/app"
+sudo rm -R DrupalCode/app
+mkdir -p DrupalCode/app
+sudo chown root:root DrupalCode/app
 docker ps
 docker volume prune -f
 docker network prune -f
@@ -7,4 +11,4 @@ docker volume ls
 docker network ls
 docker ps -a
 docker-compose build --no-cache
-docker-compose up
+docker-compose up --build
