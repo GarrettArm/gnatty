ad7d4587ebefcc1169c59add1a36e409e4221827
GarrettArm <garrett.w.armstrong@gmail.com>
2019-07-02 13:11:00 -0500

    updated versions

--- config/ssh/ssh_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "root:$SSH_PASSWORD" | chpasswd
-
-/usr/sbin/sshd -D
--- wipe_rebuild.sh
+++ wipe_rebuild.sh
@@ -1,10 +1,3 @@
 docker-compose down
-echo "##### these containers are currently running (should be none)"
-docker ps
-docker volume prune -f && docker network prune -f && docker system prune -f
-echo "##### these volumes exist (should be none)"
-docker volume ls
-echo "##### these netword exist (should be the default three)"
-docker network ls
-echo "##### these containers exist (should be none)"
-docker-compose up --build
+docker volume rm -f gnatty_db_data && docker volume rm -f gnatty_drupal_data
+docker-compose up --build -d
