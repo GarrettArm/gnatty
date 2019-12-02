e8443e634916b5e41512ac14d9be950fbc74e64b
garrett armstrong <garrett.w.armstrong@gmail.com>
2019-12-02 11:04:55 -0600

    Update wipe_rebuild.sh

--- wipe_rebuild.sh
+++ wipe_rebuild.sh
@@ -1,3 +1,3 @@
 docker-compose down
-docker volume rm -f gnatty_db_data && docker volume rm -f gnatty_drupal_data
+docker volume rm -f gnatty_db_data gnatty_drupal_data
 docker-compose up --build -d
